802ad630c7dd62aa817abc7bdb4866090132448c
nikhildev161011-tech <nikhil.dev161011@gmail.com>
2026-09-23 21:25:35 +0530

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/CricZen/sw.js', { scope: '/CricZen/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
